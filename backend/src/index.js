8a857694c2926c303c93711292da9032df9a942e
Prajwal Patil <ppbaviskar7030@gmail.com>
2025-07-05 20:26:25 +0530

    login settings progile page created

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import messageRoutes from "./routes/message.routes.js"
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"
 
 
 
@@ -13,8 +13,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 

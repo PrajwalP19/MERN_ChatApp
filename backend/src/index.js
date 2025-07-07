24ece7296103426d6b2a74584bdba5033c822a0b
Prajwal Patil <ppbaviskar7030@gmail.com>
2025-07-07 19:17:16 +0530

    changes made for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,13 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import {app, server} from './lib/socket.js'
-
+import path from "path"
 
 
 dotenv.config();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -23,7 +24,17 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
+if (process.env.NODE_ENV==="production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
+
+
 server.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
   connectDB();
 });
+
